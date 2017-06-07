116c3a70f296823330d66c10bc2fe50066d924b2
oli4123 <olivier.verhaeghe@student.howest.be>
2017-06-07 15:21:56 +0200

    index home en aanpassen + css

---
+++ DBClass.py
@@ -0,0 +1,19 @@
+from flask import render_template
+class DbClass:
+    def __init__(self):
+        import mysql.connector as connector
+
+        self.__dsn= {"host": "localhost", "user":"root", "passwd":"olivier", "db":"vissendb"}
+
+        self.__connection=connector.connect(**self.__dsn)
+        self.__cursor=self.__connection.cursor()
+    def inlog(self,gn,ww):
+        q = "select wachtwoord from tblgebruiker where gebruikersnaam='"+gn+"';"
+        self.__cursor.execute(q)
+        result = self.__cursor.fetchall()
+        self.__cursor.close()
+        for ww2 in result:
+            if(ww2[0]==ww):
+                return "home.html"
+            else:
+                return "index.html"
---
+++ aquarium.py
@@ -0,0 +1,50 @@
+from flask import Flask, render_template, request
+from DBClass import DbClass
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+@app.route('/home',methods=['POST'])
+def home():
+    gn = request.form["gn"]
+    ww = request.form["ww"]
+    db=DbClass()
+    page=db.inlog(gn,ww)
+    return render_template(page)
+
+@app.route('/home')
+def home2():
+    return render_template("home.html")
+
+@app.route('/temperatuurAanpassen')
+def aanpassen():
+    return render_template("temperatuurAanpassen.html")
+
+@app.route('/temperatuurAanpassen/zelfAangepast',methods=['POST'])
+def aangepastZelf():
+    tempZelf=request.form['zelfGraden']
+    if tempZelf>str(0):
+        commentZelf="*De temperatuur is  handmatig gewijzigd."
+    else:
+        commentZelf="*Gelieve een positief temperatuur in te geven."
+    return render_template("temperatuurAanpassen.html", commentZelf=commentZelf)
+
+@app.route('/temperatuurAanpassen/automatischAangepast',methods=['POST'])
+def aangepastAuto():
+    tempAuto=request.form['autoGraden']
+    print(tempAuto)
+    if len(tempAuto)>0:
+        commentAuto="*De temperatuur is automatisch gewijzigd."
+    else:
+        commentAuto="*Gelieve een waarde te selcteren."
+    return  render_template("temperatuurAanpassen.html", commentAuto=commentAuto)
+
+@app.route('/instellinge')
+def instellingen():
+    return render_template("instellingen.html")
+
+if __name__ == '__main__':
+    app.run()
